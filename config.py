6fd05001fdf648acb614ed67f90b7a5522764c53
abhipampacodexdop <140977883+abhipampacodexdop@users.noreply.github.com>
2023-09-17 12:27:25 +0530

    Update config.py
    Kk

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/JustAyu/Aest")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/abhipampacodexdop/Aest")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
 
